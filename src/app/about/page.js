015ff43176ed04e48ead70f7d504046134fd8505
Azzy-11 <azu11.mermona@gmail.com>
2024-06-26 08:42:58 +0900

    ♻️ ヘッドライン部分をコンポネント化_lesson3

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,35 +1,13 @@
-import Image from "next/image";
 import styles from "../page.module.css";
 import {Footer} from "../../components/Footer";
 import { Links } from "../../components/Links";
+import { Headline } from "@/components/Headline";
 
 export default function About() {
   return (
     <div>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            About Page! Get started by editing&nbsp;
-            <code className={styles.code}>src/app/page.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <Headline title="About Page" page="about/page.js" />
 
         <Links />
       </main>
---
+++ src/components/Headline.js
@@ -0,0 +1,15 @@
+import styles from "../app/page.module.css";
+
+export function Headline(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/{props.page}</code>
+        </p>
+      </div>
+    </div>
+  );
+}
